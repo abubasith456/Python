3033a9495318d628b85d292a446678d16e95d27b
Mohamed Abu Basith <mohamedabu.basith@kobil.com>
2024-12-23 20:05:38 +0530

    Code added for flow control

--- flowControl.py
+++ flowControl.py
@@ -1,9 +1,24 @@
 # Flow Controll
 
-# Comparison Operator
+# Comparison Operator (only returns true or false)
+
 # < - Less than
 # > - Greater than
 # <= - Less than or equal to
 # >= - Greater than or equal to
 # != - Not equal to
 # == - Equal to
+
+var_1 = 1 > 2   #Result: False
+var_2 = 3 < 2   #Result: False
+var_3 = 4 <= 5  #Result: True
+var_4 = 4 >= 4  #Result: True
+var_5 = 4 != 6  #Result: True
+var_6 = 4 == 5  #Result: False
+
+print(var_1)
+print(var_2)
+print(var_3)
+print(var_4)
+print(var_5)
+print(var_6)
